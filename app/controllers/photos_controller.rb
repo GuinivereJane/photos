ac7e13043b48380d5eecc80c3844aca539c325e9
Guin <guinifer.k@gmail.com>
2016-04-06 15:19:48 -0400

    added feature to create a new phtoto

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,17 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
   end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    render :text => "Saving a photo. URL #{params[:url]}, Title #{params[:title]}, Artist #{params[:artist]}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get 'photos' => 'photos#index'
+
+  post 'photos' => 'photos#create'
+  get 'photos/new' => 'photos#new'
+
+
+  get 'photos/:id' => 'photos#show', as: 'photo'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
